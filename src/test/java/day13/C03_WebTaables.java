fb8ddc45d4cb65c20410e5181fec6dd79b4b2692
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-09-23 23:25:15 +0300

    23.9.2022 V1

--- src/test/java/day13/C03_WebTaables.java
+++ src/test/java/day13/C03_WebTaables.java
@@ -1,2 +1,15 @@
-package day13;public class C03_WebTaables {
+package day13;
+
+public class C03_WebTaables {
+    /*
+Bir onceki class'taki adrese gidelim
+login() methodunu kullanarak sayfaya giris yapalim
+input olarak verilen satir ve sutun sayisina sahip
+cell'deki texti yazdiralim.
+
+int satir = 3;
+int sutun = 4;
+
+ */
+
 }
