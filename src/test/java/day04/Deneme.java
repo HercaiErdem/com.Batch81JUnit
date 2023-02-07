a7f14d8cc5782df6ada1c8642a682f3988491961
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-07 22:41:34 +0300

    v2

--- src/test/java/day04/Deneme.java
+++ src/test/java/day04/Deneme.java
@@ -13,23 +13,23 @@ public class Deneme {
 
     /*
     1. Tarayıcıyı başlatın
-2. 'http://automationexercise.com' url'sine gidin
-3. Ana sayfanın başarıyla görünür olduğunu doğrulayın
-4. 'Kayıt Ol / Giriş Yap' düğmesine tıklayın
-5. 'Yeni Kullanıcı Kaydı'nı doğrulayın! görünür
-6. Adı ve e-posta adresini girin
-7. 'Kaydol' düğmesini tıklayın
-8. 'HESAP BİLGİLERİNİ GİRİN' ifadesinin görünür olduğunu doğrulayın
-9. Doldurma ayrıntıları: Unvan, Ad, E-posta, Şifre, Doğum tarihi
-10. 'Bültenimize kaydolun!' onay kutusunu seçin.
-11. 'Ortaklarımızdan özel teklifler alın!' onay kutusunu seçin.
-12. Doldurma ayrıntıları: Ad, Soyadı, Şirket, Adres, Adres2, Ülke, Eyalet, Şehir, Posta Kodu, Cep Numarası
-13. 'Hesap Oluştur düğmesini' tıklayın
-14. 'HESAP OLUŞTURULDU!' görünür
-15. 'Devam' düğmesini tıklayın
-16. 'Kullanıcı adı olarak oturum açıldı' ifadesinin görünür olduğunu doğrulayın
-17. 'Hesabı Sil' düğmesini tıklayın
-18. 'HESAP SİLİNDİ!' görünür ve 'Devam' düğmesini tıklayın
+    2. 'http://automationexercise.com' url'sine gidin
+    3. Ana sayfanın başarıyla görünür olduğunu doğrulayın
+    4. 'Kayıt Ol / Giriş Yap' düğmesine tıklayın
+    5. 'Yeni Kullanıcı Kaydı'nı doğrulayın! görünür
+    6. Adı ve e-posta adresini girin
+    7. 'Kaydol' düğmesini tıklayın
+    8. 'HESAP BİLGİLERİNİ GİRİN' ifadesinin görünür olduğunu doğrulayın
+    9. Doldurma ayrıntıları: Unvan, Ad, E-posta, Şifre, Doğum tarihi
+    10. 'Bültenimize kaydolun!' onay kutusunu seçin.
+    11. 'Ortaklarımızdan özel teklifler alın!' onay kutusunu seçin.
+    12. Doldurma ayrıntıları: Ad, Soyadı, Şirket, Adres, Adres2, Ülke, Eyalet, Şehir, Posta Kodu, Cep Numarası
+    13. 'Hesap Oluştur düğmesini' tıklayın
+    14. 'HESAP OLUŞTURULDU!' görünür
+    15. 'Devam' düğmesini tıklayın
+    16. 'Kullanıcı adı olarak oturum açıldı' ifadesinin görünür olduğunu doğrulayın
+    17. 'Hesabı Sil' düğmesini tıklayın
+    18. 'HESAP SİLİNDİ!' görünür ve 'Devam' düğmesini tıklayın
      */
 
     public static void main(String[] args) {
@@ -40,7 +40,7 @@ public class Deneme {
         driver.manage().window().maximize();
 
         //  1. Tarayıcıyı başlatın
-        //   2. 'http://automationexercise.com' url'sine gidin
+        //  2. 'http://automationexercise.com' url'sine gidin
         driver.get("http://automationexercise.com");
 
         //   3. Ana sayfanın başarıyla görünür olduğunu doğrulayın
