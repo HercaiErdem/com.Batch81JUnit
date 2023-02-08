e2f2f7d5e63d2440e895a0488f73580652b8c9c8
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-08 19:43:05 +0300

    v6

--- src/test/java/day06/C03_Assertions.java
+++ src/test/java/day06/C03_Assertions.java
@@ -13,14 +13,14 @@ import java.time.Duration;
 
 public class C03_Assertions {
     /*
-  1) Bir class oluşturun: BestBuyAssertions
-  2) https://www.bestbuy.com/ Adresine gidin farkli test method’lari olusturarak asagidaki
-  testleri yapin
+   1) Bir class oluşturun: BestBuyAssertions
+   2) https://www.bestbuy.com/ Adresine gidin farkli test method’lari olusturarak asagidaki
+   testleri yapin
        ○ Sayfa URL’inin https://www.bestbuy.com/ ‘a esit oldugunu test edin
        ○ titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
        ○ logoTest => BestBuy logosunun görüntülendigini test edin
        ○ FrancaisLinkTest => Fransizca Linkin görüntülendiğini test edin
-   */
+    */
     static WebDriver driver;
 
     @BeforeClass
