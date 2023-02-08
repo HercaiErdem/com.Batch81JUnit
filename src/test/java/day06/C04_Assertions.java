81dbc1ce9f177b9d63b56573c34049acb7b9a144
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-08 19:48:06 +0300

    v8

--- src/test/java/day06/C04_Assertions.java
+++ src/test/java/day06/C04_Assertions.java
@@ -12,12 +12,12 @@ import java.time.Duration;
 public class C04_Assertions {
 
     /*
-     * https://www.youtube.com adresine gidin
-     * Aşağıdaki adları kullanarak 3 test metodu oluşturun ve gerekli testleri yapin
-     * titleTest => Sayfa başlığının “YouTube” oldugunu test edin
-     * imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
-     * Search Box 'in erisilebilir oldugunu test edin (isEnabled())
-     * wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+      https://www.youtube.com adresine gidin
+      Aşağıdaki adları kullanarak 3 test metodu oluşturun ve gerekli testleri yapin
+      titleTest => Sayfa başlığının “YouTube” oldugunu test edin
+      imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+      Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+      wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
      */
 
     static WebDriver driver;
@@ -38,7 +38,7 @@ public class C04_Assertions {
 
     @Test
     public void test1() {
-        //* titleTest => Sayfa başlığının “YouTube” oldugunu test edin
+        // titleTest => Sayfa başlığının “YouTube” oldugunu test edin
         String expectedTitle = "YouTube";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(expectedTitle, actualTitle);
@@ -46,20 +46,20 @@ public class C04_Assertions {
 
     @Test
     public void test2() {
-//* imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
+      // imageTest => YouTube resminin görüntülendiğini (isDisplayed()) test edin
         WebElement youtubeLogo = driver.findElement(By.xpath("(//*[@id='logo-icon'])[1]"));
         Assert.assertTrue(youtubeLogo.isDisplayed());
     }
 
     @Test
     public void test3() {
-//Search Box 'in erisilebilir oldugunu test edin (isEnabled())
+        // Search Box 'in erisilebilir oldugunu test edin (isEnabled())
         Assert.assertTrue(driver.findElement(By.xpath("(//*[@id='search'])[3]")).isEnabled());
     }
 
     @Test
     public void test4() {
-//* wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
+       // wrongTitleTest => Sayfa basliginin “youtube” olmadigini dogrulayin
         String actualTitle = driver.getTitle();
         String unExpectedTitle = "youtube";
         Assert.assertNotEquals(unExpectedTitle, actualTitle);
