b5c6581d8efbe7b4f872b08ced6acc8ec67a36e6
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-09 23:01:07 +0300

    v6

--- src/test/java/day07/C03_DropDown.java
+++ src/test/java/day07/C03_DropDown.java
@@ -20,13 +20,13 @@ public class C03_DropDown {
     /*
           ●https://www.amazon.com/ adresine gidin.
            -Test1
-           Arama kutusunun yanindaki kategori menusundeki kategori sayisinin45
+           Arama kutusunun yanindaki kategori menusundeki kategori sayisinin 45
            oldugunu testedin
 
            -Test2
-           1.Kategori menusunden Books seceneginisecin
-           2.Arama kutusuna Java yazin vearatin
-           3.Bulunan sonuc sayisiniyazdirin
+           1.Kategori menusunden Books secenegini secin
+           2.Arama kutusuna Java yazin ve aratin
+           3.Bulunan sonuc sayisini yazdirin
            4.Sonucun Java kelimesini icerdigini testedin
      */
     WebDriver driver;
@@ -81,7 +81,7 @@ public class C03_DropDown {
         System.out.println(select.getFirstSelectedOption().getText());
     // 2.Arama kutusuna Java yazin ve aratin
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java", Keys.ENTER);
-//           3.Bulunan sonuc sayisiniyazdirin
+//           3.Bulunan sonuc sayisini yazdirin
         WebElement sonucYazisi = driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
         System.out.println(sonucYazisi.getText());
 //           4.Sonucun Java kelimesini icerdigini testedin
