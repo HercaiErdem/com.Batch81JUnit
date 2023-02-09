bf415f360c7e37052d462b12cd96f87fc1448235
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-09 23:02:14 +0300

    v7

--- src/test/java/day07/C03_DropDown.java
+++ src/test/java/day07/C03_DropDown.java
@@ -81,10 +81,10 @@ public class C03_DropDown {
         System.out.println(select.getFirstSelectedOption().getText());
     // 2.Arama kutusuna Java yazin ve aratin
         driver.findElement(By.id("twotabsearchtextbox")).sendKeys("Java", Keys.ENTER);
-//           3.Bulunan sonuc sayisini yazdirin
+    // 3.Bulunan sonuc sayisini yazdirin
         WebElement sonucYazisi = driver.findElement(By.xpath("//*[@class='a-section a-spacing-small a-spacing-top-small']"));
         System.out.println(sonucYazisi.getText());
-//           4.Sonucun Java kelimesini icerdigini testedin
+    // 4.Sonucun Java kelimesini icerdigini test edin
         String expectedKelime = "Java";
         String actualSonucYazisi = sonucYazisi.getText();
         Assert.assertTrue(actualSonucYazisi.contains(expectedKelime));
