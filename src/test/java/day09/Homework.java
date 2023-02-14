db3654fcc96f6bdd39164329289a0c5851c544b1
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-14 17:14:02 +0300

    v8

--- src/test/java/day09/Homework.java
+++ src/test/java/day09/Homework.java
@@ -5,9 +5,9 @@ public class Homework {
      Ödev :
     Bir class olusturun:IframeTest02
    1)http://demo.guru99.com/test/guru99home/sitesinegidiniz
-   2)sayfadaki iframe sayısınıbulunuz.
-   3)ilk iframe’deki (Youtube) play butonunatıklayınız.
-   4)ilk iframe’den çıkıp ana sayfayadönünüz
+   2)sayfadaki iframe sayısını bulunuz.
+   3)ilk iframe’deki (Youtube) play butonuna tıklayınız.
+   4)ilk iframe’den çıkıp ana sayfaya dönünüz
    5)ikinci iframe’deki (Jmeter Made Easy) linke(https://www.guru99.com/live-selenium-project.html)
    tıklayınız
       */
