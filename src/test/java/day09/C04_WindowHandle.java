e26bfdb7fbe1a417c21b38540ea610fac1a86c20
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-14 17:20:33 +0300

    v13

--- src/test/java/day09/C04_WindowHandle.java
+++ src/test/java/day09/C04_WindowHandle.java
@@ -9,16 +9,16 @@ import java.time.*;
 import java.util.*;
 
 public class C04_WindowHandle {
-     /*
-    Tests package’inda yeni bir class olusturun: WindowHandle2
-    https://the-internet.herokuapp.com/windows adresine gidin.
-    Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
-    Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
-    Click Here butonuna basın.
-    Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
-    Sayfadaki textin “New Window” olduğunu doğrulayın.
-    Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet” olduğunu  doğrulayın.
-     */
+    /*
+   Tests package’inda yeni bir class olusturun: WindowHandle2
+   https://the-internet.herokuapp.com/windows adresine gidin.
+   Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
+   Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
+   Click Here butonuna basın.
+   Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
+   Sayfadaki textin “New Window” olduğunu doğrulayın.
+   Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet” olduğunu  doğrulayın.
+    */
     WebDriver driver;
 
     @Before
