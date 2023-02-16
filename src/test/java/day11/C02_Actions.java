31b9c3dbf17a420141f948d99c3d96b0ff7c1c28
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-16 23:26:45 +0300

    v4

--- src/test/java/day11/C02_Actions.java
+++ src/test/java/day11/C02_Actions.java
@@ -34,7 +34,5 @@ public class C02_Actions extends TestBaseBeforeAfterReview {
                 sendKeys(Keys.TAB).sendKeys(Keys.TAB)
                 .sendKeys(Keys.ENTER).perform();
 
-
     }
-
 }
