0b2ce173a41a695d5c53167c446fbc32054dd3a7
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-16 23:28:12 +0300

    v5

--- src/test/java/day11/C02_Actions.java
+++ src/test/java/day11/C02_Actions.java
@@ -1,12 +1,9 @@
 package day11;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import review.TestBaseBeforeAfterReview;
-import utilities.TestBaseBeforeAfter;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.*;
+import review.*;
 
 public class C02_Actions extends TestBaseBeforeAfterReview {
 
