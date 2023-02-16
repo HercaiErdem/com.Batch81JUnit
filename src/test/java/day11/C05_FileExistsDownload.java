5bdb98529bd1bff417209f8dbcd3ef5daf79c0d8
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-16 23:36:38 +0300

    v14

--- src/test/java/day11/C05_FileExistsDownload.java
+++ src/test/java/day11/C05_FileExistsDownload.java
@@ -1,23 +1,21 @@
 package day11;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import utilities.TestBaseBeforeAfter;
+import org.junit.*;
+import org.openqa.selenium.*;
+import utilities.*;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 public class C05_FileExistsDownload extends TestBaseBeforeAfter {
 
     @Test
     public void test1() {
 
-        //   https://the-internet.herokuapp.com/download adresine gidelim.
+        // https://the-internet.herokuapp.com/download adresine gidelim.
         driver.get("https://the-internet.herokuapp.com/download");
-        //   test.txt dosyasını indirelim
+        // test.txt dosyasını indirelim
         driver.findElement(By.xpath("//a[text()='test.txt']")).click();
-        //   Ardından isExist( )  methodunda dosyanın başarıyla indirilip indirilmediğini test edelim
+        // Ardından isExist( )  methodunda dosyanın başarıyla indirilip indirilmediğini test edelim
         String dosyaYolu = "C:\\Users\\Kayra\\Downloads\\test.txt";
         Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
         // Indirildigini konsolda goster
