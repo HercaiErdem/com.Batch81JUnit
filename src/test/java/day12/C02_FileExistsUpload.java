4b5353541e8c706501fe90a4ee43b6d9ea43e6d6
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-17 22:13:17 +0300

    v4

--- src/test/java/day12/C02_FileExistsUpload.java
+++ src/test/java/day12/C02_FileExistsUpload.java
@@ -7,23 +7,25 @@ import utilities.*;
 public class C02_FileExistsUpload extends TestBaseBeforeAfter {
     @Test
     public void test1() throws InterruptedException {
-        //  Tests packagenin altina bir class oluşturun : C05_UploadFile
-        //  https://the-internet.herokuapp.com/upload adresine gidelim
+        // Tests packagenin altina bir class oluşturun : C05_UploadFile
+        // https://the-internet.herokuapp.com/upload adresine gidelim
         driver.get("https://the-internet.herokuapp.com/upload");
-        //  chooseFile butonuna basalim
+        // chooseFile butonuna basalim
         WebElement dosyaSec = driver.findElement(By.id("file-upload"));
+
         /*
         Dosyasec butonuna direk click yapamayabiliriz çünkü windows'a müdahaleye izin vermeyebilir
         Bu yüzden Dosyasec butonunu direk locate edip sendKeys() methodu ile string değişkene atadığımız
         dosya yolumuzu göndeririz.
          */
-        //  Yuklemek istediginiz dosyayi secelim.
+
+        // Yuklemek istediginiz dosyayi secelim.
         String dosyaYolu = "C:\\Users\\Kayra\\Desktop\\text.txt";
         dosyaSec.sendKeys(dosyaYolu);
-        //  Upload butonuna basalim.
+        // Upload butonuna basalim.
         Thread.sleep(2000);
         driver.findElement(By.id("file-submit")).click();
-        //  “File Uploaded!” textinin goruntulendigini test edelim.
+        // “File Uploaded!” textinin goruntulendigini test edelim.
         Assert.assertTrue(driver.findElement(By.xpath("//h3")).isDisplayed());
 
 
