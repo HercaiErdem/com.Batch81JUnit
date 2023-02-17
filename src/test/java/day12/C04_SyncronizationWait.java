a857a636403012e79daf498ee1ac80a558e9c936
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-17 22:20:26 +0300

    v8

--- src/test/java/day12/C04_SyncronizationWait.java
+++ src/test/java/day12/C04_SyncronizationWait.java
@@ -11,13 +11,13 @@ import utilities.TestBaseBeforeAfter;
 import java.time.Duration;
 
 public class C04_SyncronizationWait extends TestBaseBeforeAfter {
- //   Bir class olusturun : EnableTest
- //   Bir metod olusturun : isEnabled()
- //   https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
- //   Textbox’in etkin olmadigini(enabled) dogrulayın
- //   Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
- //   “It’s enabled!” mesajinin goruntulendigini dogrulayın.
- //   Textbox’in etkin oldugunu(enabled) dogrulayın.
+    //   Bir class olusturun : EnableTest
+    //   Bir metod olusturun : isEnabled()
+    //   https://the-internet.herokuapp.com/dynamic_controls adresine gidin.
+    //   Textbox’in etkin olmadigini(enabled) dogrulayın
+    //   Enable butonuna tıklayın ve textbox etkin oluncaya kadar bekleyin
+    //   “It’s enabled!” mesajinin goruntulendigini dogrulayın.
+    //   Textbox’in etkin oldugunu(enabled) dogrulayın.
 
 
     @Test
@@ -35,5 +35,6 @@ public class C04_SyncronizationWait extends TestBaseBeforeAfter {
         Assert.assertTrue(driver.findElement(By.xpath("//p[text()=\"It's enabled!\"]")).isDisplayed());
         //   Textbox’in etkin oldugunu(enabled) dogrulayın.
         Assert.assertTrue(textBox.isEnabled());
+
     }
 }
