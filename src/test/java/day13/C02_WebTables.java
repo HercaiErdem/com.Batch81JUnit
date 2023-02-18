e7b69999bcc197c567ed5698c0b3a9a96e6545c1
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-18 19:54:34 +0300

    v6

--- src/test/java/day13/C02_WebTables.java
+++ src/test/java/day13/C02_WebTables.java
@@ -1,13 +1,11 @@
 package day13;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import utilities.TestBaseBeforeAfter;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.*;
+import utilities.*;
 
-import java.util.List;
+import java.util.*;
 
 public class C02_WebTables extends TestBaseBeforeAfter {
     /*
