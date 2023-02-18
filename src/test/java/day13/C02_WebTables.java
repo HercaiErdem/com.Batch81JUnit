254455a0109700ebcec15349055a2eac18ea9757
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-18 19:55:23 +0300

    v7

--- src/test/java/day13/C02_WebTables.java
+++ src/test/java/day13/C02_WebTables.java
@@ -8,20 +8,7 @@ import utilities.*;
 import java.util.*;
 
 public class C02_WebTables extends TestBaseBeforeAfter {
-    /*
-    Bir class oluşturun : C02_WebTables
-login( ) metodun oluşturun ve oturum açın.
-https://www.hotelmycamp.com/admin/HotelRoomAdmin adresine gidin
-Username : manager
-Password : Manager1!
-table( ) metodu oluşturun
-Tüm table body’sinin boyutunu(sutun sayisi) bulun.
-Table’daki tum body’I ve başlıkları(headers) konsolda yazdırın.
-printRows( ) metodu oluşturun //tr
-table body’sinde bulunan toplam satir(row) sayısını bulun.
-Table body’sinde bulunan satirlari(rows) konsolda yazdırın.
-4.satirdaki(row) elementleri konsolda yazdırın
-     */
+
     @Test
     public void test1() {
         //Bir class oluşturun : C02_WebTables
