f769ef9e773c04e72fcfe7530cafa2ecbf093428
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-19 23:17:33 +0300

    v5

--- src/test/java/day14/C03_ReadExcel.java
+++ src/test/java/day14/C03_ReadExcel.java
@@ -1,19 +1,16 @@
 package day14;
 
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.junit.Assert;
-import org.junit.Test;
+import org.apache.poi.ss.usermodel.*;
+import org.junit.*;
 
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class C03_ReadExcel {
     @Test
     public void readExcelTest1() throws IOException {
 
-     //   Belirtilen satirNo ve sutunNo degerini parametre olarak alip o cell deki datayi konsola yazdiralim
-     //   Sonucun konsolda yazilanla ayni oldugunu dogrulayin
+        //   Belirtilen satirNo ve sutunNo degerini parametre olarak alip o cell deki datayi konsola yazdiralim
+        //   Sonucun konsolda yazilanla ayni oldugunu dogrulayin
 
         int satir = 12;
         int sutun = 2;
@@ -21,19 +18,16 @@ public class C03_ReadExcel {
         String dosyaYolu = "src/resources/ulkeler.xlsx";
         FileInputStream fis = new FileInputStream(dosyaYolu);
         Workbook workbook = WorkbookFactory.create(fis);
-       //   Belirtilen satirNo ve sutunNo degerini parametre olarak alip o cell deki datayi konsola yazdiralim
+        //   Belirtilen satirNo ve sutunNo degerini parametre olarak alip o cell deki datayi konsola yazdiralim
         String actualData = workbook
                 .getSheet("Sayfa1")
-                .getRow(satir-1)
-                .getCell(sutun-1)
+                .getRow(satir - 1)
+                .getCell(sutun - 1)
                 .toString();
         System.out.println(actualData);
         //Index 0 dan basladigi icin bizden istenen satira ulasmak icin bir eksigini aliriz
         String expectedData = "Baku";
         Assert.assertTrue(actualData.contains(expectedData));
-
-
-
 
 
     }
