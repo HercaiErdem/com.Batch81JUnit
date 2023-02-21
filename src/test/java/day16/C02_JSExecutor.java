7ed147d39044fd7036fb9a81b4b6b3b2c9cff65d
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-21 12:05:40 +0300

    v2

--- src/test/java/day16/C02_JSExecutor.java
+++ src/test/java/day16/C02_JSExecutor.java
@@ -1,10 +1,8 @@
 package day16;
 
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
-import utilities.TestBaseBeforeAfter;
+import org.junit.*;
+import org.openqa.selenium.*;
+import utilities.*;
 
 public class C02_JSExecutor extends TestBaseBeforeAfter {
     @Test
