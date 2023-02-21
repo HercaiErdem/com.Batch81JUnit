c16e75c0e81f7b97a0b07a9edbfc641c9fdbe73d
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-21 12:06:50 +0300

    v3

--- src/test/java/day16/Homework.java
+++ src/test/java/day16/Homework.java
@@ -6,4 +6,5 @@ public class Homework {
     //2 farkli test method’u olusturarak actions classi ve Js Executor kullanarak asagidaki
     //oda turlerinden  ust sira ortadaki odayi tiklayin
     //istediginiz oda inceleme sayfasi acildigini test  edin
+
 }
