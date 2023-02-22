71344bf1678a78b6298d12c9b61c4c740e40cb90
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-22 14:37:15 +0300

    22.02.2023 v1

--- src/test/java/practice/C01_GMBankLogin.java
+++ src/test/java/practice/C01_GMBankLogin.java
@@ -1,11 +1,8 @@
 package practice;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
 
 public class C01_GMBankLogin {
 
