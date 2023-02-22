3d3e4aef96b6309182bede239c7f3ea25a7d106f
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-22 15:00:40 +0300

    v6

--- src/test/java/practice/C03_LoginTest.java
+++ src/test/java/practice/C03_LoginTest.java
@@ -1,13 +1,9 @@
 package practice;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
 
 import java.time.Duration;
 
