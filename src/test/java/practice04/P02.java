9eae74a41f4f7f21dbdfc40c8992485bfc60c72f
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-23 15:38:11 +0300

    v6

--- src/test/java/practice04/P02.java
+++ src/test/java/practice04/P02.java
@@ -7,7 +7,7 @@ import java.nio.file.*;
 public class P02 {
     @Test
     public void verify() {
-        //Verify that we have pom.xml file in our project => please try in 4 min s
+        //   Verify that we have pom.xml file in our project => please try in 4 min s
 
         //   String path = "C:\\Batch81\\com.Batch81JUnit\\pom.xml";
 
