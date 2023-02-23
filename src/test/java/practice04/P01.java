5a465dcf15d947400a1f2392a5aaf0542bdbe3fc
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-23 15:23:30 +0300

    v2

--- src/test/java/practice04/P01.java
+++ src/test/java/practice04/P01.java
@@ -9,10 +9,10 @@ import java.util.List;
 
 public class P01 extends TestBaseBeforeAfter {
     // go to the URL https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/
-// ikinci emojiye tıklayın (hayvan emojisini seçin)
-// tüm hayvan emojilerine tıklayın
-// formu doldurun (dilediğinizi yazabilirsiniz)
-// apply butonuna tıklayın
+    // ikinci emojiye tıklayın (hayvan emojisini seçin)
+    // tüm hayvan emojilerine tıklayın
+    // formu doldurun (dilediğinizi yazabilirsiniz)
+    // apply butonuna tıklayın
 
 
     @Test
