98cf366fe69f8c477cb3fd9adf53001292007e7e
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-24 15:31:30 +0300

    v3

--- src/test/java/review/ActionsReview01.java
+++ src/test/java/review/ActionsReview01.java
@@ -16,7 +16,7 @@ public class ActionsReview01 extends ReviewBeforeAfterClassTestBase {
         // - Amazon sayfasina gidelim
         driver.get("https://www.amazon.com");
         // - Account menusunden create a list linkine tiklayalim
-        WebElement accountBox= driver.findElement(By.xpath("//*[@id='nav-link-accountList']"));
+        WebElement accountBox = driver.findElement(By.xpath("//*[@id='nav-link-accountList']"));
         Actions action = new Actions(driver);
         action.moveToElement(accountBox).perform();
         WebElement createAListButton = driver.findElement(By.xpath("//span[text()='Create a List']"));
