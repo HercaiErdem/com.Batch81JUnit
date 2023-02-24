25ad03dd72748899f2dc12eb0ed9eba92c8c45d9
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-24 15:52:56 +0300

    v9

--- src/test/java/review/AllertsCalismasi01.java
+++ src/test/java/review/AllertsCalismasi01.java
@@ -11,18 +11,18 @@ public class AllertsCalismasi01 {
 
     /*
     Bir class olusturun: Alerts
-https://the-internet.herokuapp.com/javascript_alerts adresine gidin.
-Bir metod olusturun: acceptAlert
-1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının
- “You successfully clicked an alert” oldugunu test edin.
+    https://the-internet.herokuapp.com/javascript_alerts adresine gidin.
+    Bir metod olusturun: acceptAlert
+    1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının
+     “You successfully clicked an alert” oldugunu test edin.
 
-Bir metod olusturun: dismissAlert
-2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının
-“successfuly” icermedigini test edin.
+    Bir metod olusturun: dismissAlert
+    2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının
+    “successfuly” icermedigini test edin.
 
-Bir metod olusturun: sendKeysAlert
-3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna
-tıklayın ve result mesajında isminizin görüntülendiğini doğrulayın.
+    Bir metod olusturun: sendKeysAlert
+    3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna
+    tıklayın ve result mesajında isminizin görüntülendiğini doğrulayın.
      */
 
     static WebDriver driver;
