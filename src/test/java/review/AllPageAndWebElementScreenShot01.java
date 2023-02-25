09e04879e266309b2557dbf26626076970cca52d
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-02-25 21:28:01 +0300

    v2

--- src/test/java/review/AllPageAndWebElementScreenShot01.java
+++ src/test/java/review/AllPageAndWebElementScreenShot01.java
@@ -1,23 +1,22 @@
 package review;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
+import org.apache.commons.io.*;
+import org.junit.*;
 import org.openqa.selenium.*;
-import utilities.TestBaseBeforeClassAfterClass;
+import utilities.*;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class AllPageAndWebElementScreenShot01 extends TestBaseBeforeClassAfterClass {
 
-    //hepsiburada.com a gidin https://www.hepsiburada.com
+    // hepsiburada.com a gidin https://www.hepsiburada.com
     // Dell Laptop aratalim
     // Tum sayfanin screen shor ini alalim
 
 
     @Test
     public void test01() throws IOException {
-        //hepsiburada.com a gidin https://www.hepsiburada.com
+        // hepsiburada.com a gidin https://www.hepsiburada.com
         driver.get("https://hepsiburada.com");
         // Dell Laptop aratalim
         driver.findElement(By.xpath("//*[@type='text']"))
