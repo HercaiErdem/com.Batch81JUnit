5e401ac2ab38f0fd8abcabf4593e691e280f7aea
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-01 14:10:18 +0300

    v2

--- src/test/java/review/ReadExcelPractice01.java
+++ src/test/java/review/ReadExcelPractice01.java
@@ -14,33 +14,22 @@ public class ReadExcelPractice01 {
     @Test
     public void readExcelTest1() throws IOException {
         //- Dosya yolunu bir String degiskene atayalim
-        String dosyaYolu= "src/resources/ulkeler.xlsx";
+        String dosyaYolu = "src/resources/ulkeler.xlsx";
         //- FileInputStream objesi olusturup,parametre olarak dosya yolunu girelim
-        FileInputStream fis =new FileInputStream(dosyaYolu);
+        FileInputStream fis = new FileInputStream(dosyaYolu);
         //- Workbook objesi olusturalim,parameter olarak fileInputStream objesini girelim
         Workbook workbook = WorkbookFactory.create(fis);
-            //   Belirtilen satirNo ve sutunNo degerini parametre olarak alip o cell deki datayi konsola yazdiralim
-               int satir = 8;
-                int sutun = 3;
+        //   Belirtilen satirNo ve sutunNo degerini parametre olarak alip o cell deki datayi konsola yazdiralim
+        int satir = 8;
+        int sutun = 3;
 
-                String acualData = workbook.getSheet("Sayfa1").getRow(satir-1).getCell(sutun-1)
-                        .toString();
+        String acualData = workbook.getSheet("Sayfa1").getRow(satir - 1).getCell(sutun - 1)
+                .toString();
         System.out.println(acualData);
 
         //   Sonucun konsolda yazilanla ayni oldugunu dogrulayin
         String expectedResult = "Arjantin";
         Assert.assertTrue(acualData.contains(expectedResult));
-
-
-
-
-
-
-
-
-
-
-
 
 
     }
