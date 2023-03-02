4bb4b2e8202d73c010940f7a4790ae50098a4a55
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-02 15:00:06 +0300

    v6

--- src/test/java/review/TestBaseBeforeAfterReview.java
+++ src/test/java/review/TestBaseBeforeAfterReview.java
@@ -7,11 +7,12 @@ public class TestBaseBeforeAfterReview extends ReviewBeforeAfterTestBase {
 
 
     @Test
-    public void test01(){
+    public void test01() {
         driver.get("https://www.techproeducation.com");
     }
+
 }
- /*
- Child class in Parent class a ulaşması için “extends” keyword unu kullandık.
- Bu sayede özellik ve metotları inharence yaprik.
-  */
+         /*
+       Child class in Parent class a ulaşması için “extends” keyword unu kullandık.
+       Bu sayede özellik ve metotları inharence yaptik.
+        */
