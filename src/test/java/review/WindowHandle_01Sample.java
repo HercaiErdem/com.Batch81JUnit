92139c9e5d872f910da15c99a2122f366a23f0f9
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-03 15:42:03 +0300

    v5

--- src/test/java/review/WindowHandle_01Sample.java
+++ src/test/java/review/WindowHandle_01Sample.java
@@ -9,16 +9,6 @@ import java.time.*;
 
 public class WindowHandle_01Sample {
 
-    //1- Amazon Sayfasina girelim
-    //2- Url'nin amazon içerdiğini test edelim
-    //3- Yeni bir pencere açıp bestbuy anasayfasına gidelim.(https://www.bestbuy.com)
-    //4- title'in Best Buy içerdiğini test edelim
-    //5- İlk sayfaya dönüp sayfada java aratalım
-    //6- Arama sonuclarının java içerdiğini test edelim
-    //7- Yeniden bestbuy sayfasına gidelim
-    //8- Logonun görünürlüğünü test edelim
-    //9- Sayfaları Kapatalım
-
     static WebDriver driver;
 
     @BeforeClass
